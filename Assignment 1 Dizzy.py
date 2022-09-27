46b13db3f8017c367e4481c63308169b93ebec7d
dizzyvduijn <112895782+dizzyvduijn@users.noreply.github.com>
2022-09-27 17:53:44 +0200

    functions added to main
    - get_nfeat_cols
    - check_pure_node
    - create_child

---
+++ Assignment 1 Dizzy.py
@@ -0,0 +1,105 @@
+from pickle import TRUE
+import numpy as np
+
+
+
+# 1.  
+# def nmin_check(obs, nmin):
+
+#     """checks if y has more that nmin values. If not, node must not be splitted and tree will not
+#    be grown further. boolean output.
+
+#     # Args:
+#     # obs: column of an array of dataset
+#     # nmin: parameter that sets the minimum number of observations in data required to make a split
+
+#     # Returns:
+#     # Boolean, returns true if there are more observations than nmin."""
+
+#     if len(obs) > nmin:
+#         return True
+#     else:
+#         return False
+
+# # check
+# array = np.array([1,0,1,1])
+# min_value = nmin_check(array, 5)
+# print(min_value)
+
+
+
+# def check_impurity(arr):
+
+#     """Function that checks the impurity of a vector, using the Gini index
+
+#     # Args:
+#     # arr: column of an array of dataset
+
+#     # Returns:impurity
+#     # impurity."""
+
+#     size = len(arr)
+#     counts = dict(zip(*np.unique(arr, return_counts=True)))
+
+#     if (0 in counts) & (1 in counts):
+#         return (counts[0] / size) * (counts[1] / size)
+#     else:
+#         return 0
+
+# #check
+# array=np.array([1,0,1,1,1,0,0,1,1,0,1])
+# array=np.array([0,0,0,0,0,0,0,0,0,0,0])
+# check_impurity(array)
+
+
+
+
+# def check_pure_node(arr):
+
+#     """Function that checks whether vector is pure, using the Gini index
+
+#     # Args:
+#     # arr: column of an array of dataset
+
+#     # Returns: Boolean."""
+#     result = np.all(arr == arr[0])
+#     if result:
+#         return True
+#     else:
+#         return False
+
+
+# array=np.array([1,0,1,1,1,0,0,1,1,0,1])
+
+# array=np.array([0,0,0,0,0,0,0,0,0,0,0])
+# print(check_pure_node(array))
+
+
+   
+
+def get_nfeat_cols(num_cols, nfeat):
+
+    """function that returns a random subset (size nfeat) of the number of columns of a dataframr. Nfeat cannot be larger than the number of columns
+
+      Args:
+      total_col_nums: number of columns of a matrix/dataframe
+      nfeat: parameter that determines the number of features that will be used for determining a split of the data
+
+      Returns:
+      random subset of number of columns.
+      """
+    if nfeat > num_cols:
+        print("nfeat cannot be larger than the number of attributes")
+    else:
+        cols_nfeat = np.random.choice(np.arange(0, num_cols), size=nfeat, replace=False)
+        return cols_nfeat
+
+#check
+credit_data = np.genfromtxt('data.txt', delimiter=',', skip_header=True)
+cols = get_nfeat_cols(np.shape(credit_data)[1], 6)
+print(cols)
+
+
+
+
+
--- dizzy.py
+++ dizzy.py
@@ -1,3 +1,4 @@
+from turtle import right
 import numpy as np
 from node import Node
 
@@ -8,16 +9,56 @@ c = np.shape(data)[1] # number of columns
 X = data[:,:c-1]
 y = data[:,c-1]
 
+
 node = Node(X, y)
 
 
-print("Dit is het X frame in de node")
-print(node.X)
+# print("Dit is het X frame in de node")
+# print(node.X)
 
-print("Dit zijn de y labels in de node")
-print(node.y)
-
+# print("Dit zijn de y labels in de node")
+# print(node.y)
+# print("======================")
 
 # Je kunt nu de Node "node" gebruiken als input.
 # Ik heb even wat geprint zodat je een idee krijgt
 # Meer kun je vinden in het bestand node.py
+
+
+# create_childs(Node node, int best_col, double split_val)
+
+#     function that, given the split_col and split_val, splits the data set in two parts and creates two new Node 
+#     objects (including their X, y attributes only). These objects are added to the node as left/right child objects.
+
+
+def create_childs(node, best_col, split_val):
+    left_child_attributes = node.X[node.X[:, best_col]<split_val, ]
+    left_child_classification = node.y[node.X[:, best_col]<split_val, ]
+    right_child_attributes = node.X[node.X[:, best_col]>split_val, ]
+    right_child_classification = node.y[node.X[:, best_col]>split_val, ]
+
+    print("left child attributes:")
+    print(left_child_attributes)
+    print("left_child classification")
+    print(left_child_classification)
+
+    print("right child attributes")
+    print(right_child_attributes)
+    print("right child classification")
+    print(right_child_classification)
+
+    node.set_left_child(left_child_attributes, left_child_classification)
+    node.set_right_child(right_child_attributes, right_child_classification)
+    print(node.left_child.X)
+
+    return node
+    
+
+
+best_col = 3
+split_val = 55
+node = create_childs(node, best_col, split_val)
+
+print("============================")
+
+print(node.left_child.X)

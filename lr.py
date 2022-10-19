8841f88b2a1bf3b55146b2199e9297706a48e12a
joelplantinga <joelplantinga@hotmail.com>
2022-10-19 12:47:07 +0200

    preproc

--- lr.py
+++ lr.py
@@ -13,73 +13,125 @@ import numpy as np
 
 
 def create_dataset():
-    path=r"C:\Users\Parsa\Desktop\University\Data Mining\Project\Classification_treev3\Classification_tree\op_spam_v1.4\op_spam_v1.4\negative_polarity"
+    # path=r"C:\Users\Parsa\Desktop\University\Data Mining\Project\Classification_treev3\Classification_tree\op_spam_v1.4\op_spam_v1.4\negative_polarity"
+    dirname = os.path.dirname(__file__)
+    path= os.path.join(dirname, r"op_spam_v1.4/op_spam_v1.4/negative_polarity")
     n=1
-    lst_comment=[]
-    lst_class=[]
-    lst_type=[]
+    df = pd.DataFrame(columns=['class','comment','type', 'filename'])
     # find deceptive comment in dataset and give type(test or train) to it 
     while n<6:
+        lst_comment=[]
+        lst_class=[]
+        lst_type=[]
+        lst_file=[]
+
         for child in os.scandir(path):
             if 'deceptive' in child.path:
-                    for file in os.listdir(child.path+r'\fold{}'.format(n)):
+
+                    for file in os.listdir(child.path+r'/fold{}'.format(n)):
                         if file.endswith(".txt"):
-                            with open(child.path+r'\fold{}'.format(n)+"\\"+file) as f:
+                            with open(child.path+r'/fold{}'.format(n)+"/"+file) as f:
                                 lines=f.read()
                                 lst_comment.append(lines)
                                 lst_class.append('deceptive')
-        lst_type.clear()                        
+                                lst_file.append(file)
+        lst_type.clear()    
         if n==5:
             for i in range(len(lst_class)):
                 lst_type.append("test")
-            dt=pd.DataFrame(pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type}))
+            # dt=pd.DataFrame(pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type}))
+            df = pd.concat((df, pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type, 'filename':lst_file})))
         else:
+            
             for i in range(len(lst_class)):
                 lst_type.append("train")
-            dt=pd.DataFrame(pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type}))
-            
+            # dt=pd.DataFrame(pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type}))
+            df = pd.concat((df, pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type, 'filename':lst_file})))
+
         n=n+1
     n=1
     
     # find truthful comment in dataset and give type(test or train) to it
     while n<6:
+        lst_comment=[]
+        lst_class=[]
+        lst_type=[]
+        lst_file=[]
+
         for child in os.scandir(path):
             if 'truthful' in child.path:
-                    for file in os.listdir(child.path+r'\fold{}'.format(n)):
+                    for file in os.listdir(child.path+r'/fold{}'.format(n)):
                         if file.endswith(".txt"):
-                            with open(child.path+r'\fold{}'.format(n)+"\\"+file) as f:
+                            with open(child.path+r'/fold{}'.format(n)+"/"+file) as f:
                                 lines=f.read()
                                 lst_comment.append(lines)
-                                lst_class.append('truthful')   
+                                lst_class.append('truthful')
+                                lst_file.append(file)
+   
         lst_type.clear()
         if n==5:
             for i in range(0,len(lst_class)):
                 lst_type.append("test")
-            dt=pd.DataFrame(pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type}))
+            # dt=pd.DataFrame(pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type}))
+            df = pd.concat((df, pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type, 'filename':lst_file})))
+
         else:
             for i in range(0,len(lst_class)):
                 lst_type.append("train")
-            dt=pd.DataFrame(pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type}))
-        n=n+1
-    return dt
+            # dt=pd.DataFrame(pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type}))
+            df = pd.concat((df, pd.DataFrame({'class':lst_class,'comment':lst_comment,'type':lst_type, 'filename':lst_file})))
 
-def encode_label(dataset):
-    le= preprocessing.LabelEncoder()
-    le.fit(dataset)
-    #encode label
-    labels=le.transform(dataset)
-    return labels    
+
+        n=n+1
+    return df.reset_index()
+
 def vectorize(dt):
     #vectorize feactures and labels to change the text to the number
-    vectorizer = CountVectorizer(min_df=5, encoding='latin-1', ngram_range=(1, 5), stop_words='english')
-    features = pd.DataFrame(vectorizer.fit_transform(dt['comment'].values).toarray().astype(np.float32)).values
-    labels=encode_label(dt['class'])
-     #prepare dataset
     
-    return dt
-dt= create_dataset()
-print(dt['comment'])
-dt=vectorize(dt)
+    vectorizer = CountVectorizer(min_df=5, encoding='latin-1', ngram_range=(1, 2), stop_words='english')
+
+    vec = vectorizer.fit_transform(dt['comment']).toarray()#.astype(np.float32)
+
+    vectorized = pd.DataFrame(data=vec, columns=vectorizer.get_feature_names_out())
+
+    vectorized.insert(0, 'class_label', dt['class'])
+    vectorized.insert(1, 'set_type', dt['type'])
+    vectorized.insert(2, 'original_file', dt['filename'])
+
+    return vectorized
+
+
+
+# dt = create_dataset()
+
+# dt.to_csv('original.csv')
+
+
+dt = pd.read_csv('original.csv')
+dt = vectorize(dt)
+
+dt['class_label'] = dt['class_label'].transform(lambda x: 0 if x == 'deceptive' else 1)
+
+dt = dt.drop(['original_file'], axis=1)
+
+print(dt.head)
+dt.to_csv('converted.csv')
+
+X_train = dt.loc[dt['set_type'] == 'train', ~dt.columns.isin(['class_label', 'set_type'])]
+y_train = dt.loc[dt['set_type'] == 'train', dt.columns.isin(['class_label'])]
+
+X_test = dt.loc[dt['set_type'] == 'test', ~dt.columns.isin(['class_label', 'set_type'])]
+y_test = dt.loc[dt['set_type'] == 'test', dt.columns.isin(['class_label'])]
+
+print(y_test)
+
+
+
+print(len(dt))
+
+
+
+
 # x_train=dt_train['comment']
 # y_train=dt_train['class']
 # lr=LogisticRegression(penalty="l2")

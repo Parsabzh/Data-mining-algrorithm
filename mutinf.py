5a49a80899d7c9a6279969ac863f3aacecbdca1f
dizzyvduijn <112895782+dizzyvduijn@users.noreply.github.com>
2022-10-31 09:41:20 +0100

    -

--- mutinf.py
+++ mutinf.py
@@ -4,10 +4,12 @@ import pandas as pd
 
 
 dt_binary = pd.read_csv(f"data/converted_binary_unigram.csv")
+dt_binary = dt_binary.drop('set_type', axis=1)
+print(dt_binary)
 
-corr = dt_binary[dt_binary.columns[1:]].corr()['class'][:]
+corr = dt_binary[dt_binary.columns[1:]].corr()['class_label'][:]
 
-def sorting(numbers_array):
-     return sorted(numbers_array, key=abs, reverse=True)
+# def sorting(numbers_array):
+#      return sorted(numbers_array, key=abs, reverse=True)
 
-print(sorting(corr[:30]))
+# print(sorting(corr[:30]))
